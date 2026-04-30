d6e9dcc19b482ad17b6cd2624a53dbbc4506abe9
Abdal <abdal0672@gmail.com>
2026-04-30 08:55:41 +0530

    message

--- script.js
+++ script.js
@@ -59,3 +59,9 @@ window.addEventListener('scroll', () => {
     backToTopBtn.classList.remove('show');
   }
 });
+const toggle = document.getElementById("nav-toggle");
+const menu = document.getElementById("nav-menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
